9009b09264a9d39c6cb03997d634c2f3335a7518
hichaniko <hichaniko1127@gmail.com>
2023-08-01 12:09:02 +0900

    postsコントローラー作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,6 @@
 class PostsController < ApplicationController
 
-  def index  # indexアクションを定義した
-    @posts = Post.all  # すべてのレコードを@postsに代入
-  end
-
-  def new
-  end
-
-  def create
-    Post.create(content: params[:content])
-    redirect_to "/posts"
+  def index  
   end
 
 end
